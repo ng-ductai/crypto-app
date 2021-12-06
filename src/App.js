cc6085616a932836a1b8942ad18f957ad1c1aecc
Nguyen Duc Tai <ductai2982@gmail.com>
2021-12-07 02:36:55 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography } from 'antd';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navbar from './components/Navbar'
+import Homepage from './components/Homepage';
+import Exchanges from './components/Exchanges';
+import Cryptocurrencies from './components/Cryptocurrencies';
+import CryptoDetails from './components/CryptoDetails';
+import News from './components/News';
+
+const App = () => (
+  <div className="app">
+    <Router>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />}/>
+              <Route exact path="/exchanges" element={<Exchanges />}/>
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />}/>
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />}/>
+              <Route exact path="/news" element={<News />}/>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021 
+            <Link to="/">
+              Cryptoverse Inc.
+            </Link> <br />
+            All Rights Reserved.
+          </Typography.Title>          
+        </div>
+      </div>
+    </Router>
+  </div>
+);
 
 export default App;
